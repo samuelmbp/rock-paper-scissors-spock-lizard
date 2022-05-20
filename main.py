416cd51f73997c77a418119124aea61f4a60af8a
Samuel Raducan <samuel.thembp@gmail.com>
2022-05-20 09:35:15 +0100

    Refactor logic in the show_winner function

--- main.py
+++ main.py
@@ -22,28 +22,38 @@ import random
 
 CHOICES = ["rock", "paper", "scissors"]
 
+BEATS = {
+    "rock": ["scissors"],
+    "paper": ["rock"],
+    "scissors": ["paper"],
+}
+
+MESSAGES = {
+    ("rock", "scissors"): "smashes",
+    ("paper", "rock"): "covers",
+    ("scissors", "paper"): "cut",
+}
+
 
 def show_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print(f"It's a tie! Both users chose '{user_choice}' ")
-    elif user_choice == "rock":
-        if computer_choice == "scissors":
-            print("Rock smashes scissors, you win!")
-        else:
-            print("Paper covers rock, you lose!")
-    elif user_choice == "paper":
-        if computer_choice == "rock":
-            print("Paper covers rock, you win!")
-        else:
-            print("Scissors cut paper, you lose!")
-    elif user_choice == "scissors":
-        if computer_choice == "paper":
-            print("Scissors covers paper, you win!")
-        else:
-            print("Rock smashes scissors, you lose!")
-    else:
+    elif user_choice not in BEATS.keys():
         print(f"\nYou typed '{user_choice}' which isn't a valid throw.")
+    else:
+        # BEATS[user_choice] is the list of things user_choice wins over
+        user_wins = computer_choice in BEATS[user_choice]
 
+        if user_wins:
+            verb = MESSAGES[(user_choice, computer_choice)]
+            print(
+                f"{user_choice.capitalize()} {verb} {computer_choice}, you win!"
+            )
+        else:
+            verb = MESSAGES[(computer_choice, user_choice)]
+            print(
+                f"{computer_choice.capitalize()} {verb} {user_choice}, you lose!"
+            )
 
 while True:
     print("Make a throw")
